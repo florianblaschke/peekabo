049f81c8561a6884e292b193019ca60d2ef80400
Florian Blaschke <florianblaschke@web.de>
2023-04-14 11:46:00 +0200

    it works

--- javascript.js
+++ javascript.js
@@ -1,5 +1,37 @@
-const divone = document.querySelector('[data-js="card1"]');
+const div1 = document.querySelector('[data-js="card1"]');
+const div2 = document.querySelector('[data-js="card2"]');
+const div3 = document.querySelector('[data-js="card3"]');
+const div4 = document.querySelector('[data-js="card4"]');
+const div5 = document.querySelector('[data-js="card5"]');
+const div6 = document.querySelector('[data-js="card6"]');
+const div7 = document.querySelector('[data-js="card7"]');
+const div8 = document.querySelector('[data-js="card8"]');
+const div9 = document.querySelector('[data-js="card9"]');
+const div10 = document.querySelector('[data-js="card10"]');
+const div11 = document.querySelector('[data-js="card11"]');
+const div12 = document.querySelector('[data-js="card12"]');
+const div13 = document.querySelector('[data-js="card13"]');
+const div14 = document.querySelector('[data-js="card14"]');
+const div15 = document.querySelector('[data-js="card15"]');
+const div16 = document.querySelector('[data-js="card16"]');
+
 const text = document.getElementById("div1");
+const text2 = document.getElementById("div2");
+const text3 = document.getElementById("div3");
+const text4 = document.getElementById("div4");
+const text5 = document.getElementById("div5");
+const text6 = document.getElementById("div6");
+const text7 = document.getElementById("div7");
+const text8 = document.getElementById("div8");
+const text9 = document.getElementById("div9");
+const text10 = document.getElementById("div10");
+const text11 = document.getElementById("div11");
+const text12 = document.getElementById("div12");
+const text13 = document.getElementById("div13");
+const text14 = document.getElementById("div14");
+const text15 = document.getElementById("div15");
+const text16 = document.getElementById("div16");
+
 const body = document.getElementById("body");
 const button = document.querySelector('[data-js="kiss"]');
 
@@ -7,6 +39,66 @@ const button = document.querySelector('[data-js="kiss"]');
 //   body.classList.toggle("black");
 // });
 
-divone.addEventListener("click", () => {
+div1.addEventListener("click", () => {
   text.classList.toggle("visible");
+});
+
+div2.addEventListener("click", () => {
+  text2.classList.toggle("visible");
+});
+
+div3.addEventListener("click", () => {
+  text3.classList.toggle("visible");
+});
+
+div4.addEventListener("click", () => {
+  text4.classList.toggle("visible");
+});
+
+div5.addEventListener("click", () => {
+  text5.classList.toggle("visible");
+});
+
+div6.addEventListener("click", () => {
+  text6.classList.toggle("visible");
+});
+
+div7.addEventListener("click", () => {
+  text7.classList.toggle("visible");
+});
+
+div8.addEventListener("click", () => {
+  text8.classList.toggle("visible");
+});
+
+div9.addEventListener("click", () => {
+  text9.classList.toggle("visible");
+});
+
+div10.addEventListener("click", () => {
+  text10.classList.toggle("visible");
+});
+
+div11.addEventListener("click", () => {
+  text11.classList.toggle("visible");
+});
+
+div12.addEventListener("click", () => {
+  text12.classList.toggle("visible");
+});
+
+div13.addEventListener("click", () => {
+  text13.classList.toggle("visible");
+});
+
+div14.addEventListener("click", () => {
+  text14.classList.toggle("visible");
+});
+
+div15.addEventListener("click", () => {
+  text15.classList.toggle("visible");
+});
+
+div16.addEventListener("click", () => {
+  text16.classList.toggle("visible");
 });
